fb54840efae43765105f267649eeee36c8be73ec
etjecode <feyzan.aydin@gmail.com>
2025-09-06 16:14:03 +0300

    feat(router): add Vue Router 3 with HomeView as default route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
 
-// Global register, plugin vs. eklemelerini burada yapacağız
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount("#app")
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Home Page</h1>
+    <p class="mt-2 text-gray-600">Welcome to the Emlak Randevu Uygulaması!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+}
+</script>
